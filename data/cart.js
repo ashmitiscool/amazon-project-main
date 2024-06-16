26144c3630958f030097313e873ac1ad2de03272
Ashmit Verma <ashmitv444@gmail.com>
2024-06-17 01:25:34 +0530

    Added default values to cart (for development for checkout.js), generated the html for ONLY the quantity of the product in the cart

--- data/cart.js
+++ data/cart.js
@@ -10,7 +10,17 @@ array[
   }
 ] 
 */
-export let cart = [];
+// temporarily adding default values to the cart just for development of checkout.js
+export let cart = [
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+  {
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    quantity: 1,
+  },
+];
 
 export function getCartQuantity() {
   let cartQuantity = 0;
